efdddda8680cd1717f076571ad9d82eb1e17eefd
manjunath v <manjummmvvv18@gmail.com>
2013-07-26 15:21:54 +0000

    added sync

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  var content = fs.readFile('./index.html');
+  var content = fs.readFileSync('./index.html');
   response.send(content.toString());
 
 });
